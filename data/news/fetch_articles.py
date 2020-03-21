1e4a7d233bb9993d7d3c9dd4c925dfc1fe7ed915
Mohammed Furqan Rahamath M <mohammed@moonraft.com>
2020-03-21 14:10:10 -0400

    Feat: Scripts to fetch meta-data and scrape articles

---
+++ data/news/fetch_articles.py
@@ -0,0 +1,64 @@
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+import csv
+import time
+from tqdm import tqdm
+import os
+import os.path
+
+# Total range of years
+start_year = 1981
+end_year = 2019
+
+# Customise which year, quarter and row to start from and which year to end at
+start_from_year = 2006
+start_from_quarter = 4
+start_from_row = 69
+stop_at_year = 2020
+
+for year in range(end_year - start_year):
+    # Stop at the mentioned year
+    if start_year+year == stop_at_year:
+        break
+    
+    # Skip fetching previously covered data
+    if (start_year+year < start_from_year):
+        continue
+    
+    out_file_name = 'articles_{}.csv'.format(start_year+year)
+    columns = ['_id','url','word_count','section','date','type','headline','abstract','article']
+    with open(out_file_name, 'a+') as out_file:
+        writer = csv.writer(out_file)
+        
+        # If file is created new or empty, write column names first
+        if os.path.isfile(out_file_name):
+            if (os.stat(out_file_name).st_size == 0):
+                writer.writerow(columns)
+        else:
+            writer.writerow(columns)
+        
+        for quarter in range(4):
+            # Skip Quarters as per the customisations
+            if (start_year+year == start_from_year) and (quarter < start_from_quarter-1):
+                continue
+            with open('data/articles_metadata_{}_{}.csv'.format(start_year+year, quarter+1)) as meta_file:
+                data = csv.DictReader(meta_file)
+                num_articles = 2000
+                print('{} - {}'.format(start_year+year, quarter+1))
+                pbar = tqdm(total=num_articles) # Only to show the progress bar
+                for index, row in enumerate(data):
+                    # Skip rows as per the customisations
+                    if (start_year+year == start_from_year) and (quarter == start_from_quarter-1) and (index < start_from_row):
+                        pbar.update(1)
+                        continue
+                    page = requests.get(row["url"]).text
+                    soup = BeautifulSoup(page, 'html.parser')
+                    article_body = soup.select('section[name=articleBody] p')
+                    content = ""
+                    for paragraph in article_body:
+                        content += (' ' + paragraph.get_text()).replace('\n', '')
+                    row['article'] = content
+                    pbar.update(1)
+                    writer.writerow([row['_id'],row['url'],row['word_count'],row['section'],row['date'],row['type'],row['headline'].replace('\n', ''),row['abstract'].replace('\n', ''),row['article']])
+                pbar.close()
---
+++ data/news/fetch_meta.py
@@ -0,0 +1,138 @@
+import requests
+import math
+import csv
+import sys
+import time
+from tqdm import tqdm
+
+# Data to be used while querying
+url = 'https://api.nytimes.com/svc/search/v2/articlesearch.json?'
+params = {
+    "api-key": "k7d8PIJozXNxppfbVMcmqKXh1j2is4BQ",
+    "fq": {
+        "source": ["The New York Times"],
+        "section_name": ["Your Money", "Job Market", "Business", "World", "Business Day", "Technology", "Financial", "Politics"],
+        "document_type": ["article"]
+    }
+}
+start_year = 2015
+end_year = 2019
+api_keys = [
+    "k7d8PIJozXNxppfbVMcmqKXh1j2is4BQ", #northeastern
+    "srRafQT5F9h2QbSg2K7o5yvHyGGm6VXB", #MS
+    "ANAG6XCb39P2QjueyGhHh0DRo6YlgO3I", #Personal
+    "qUBjJKERphRkfn26FXuShub0zNSUM5qI", #northeasterngmail
+    "5IGEJJjATG4kG5jUR7ToPsNKqLNTK1xw", #Personalfame
+    "G38sQ8GrIeOAcRWrWK0ioJ5Sd4fuA3mV", #Personaljobs
+    "W0J9rkchzD6YKgtAgm8qf8Unh42cgSWD", #Private
+    "0mXrz72SAxcLqdSubOZDMNTU40qflb6s"  #Private2
+]
+
+# Form the URL with the URL data
+def generateURL(url, params):
+    num_params = len(params.keys())
+    for idx, param in enumerate(params):
+        param_value = ""
+        if param != 'fq':
+            param_value = params[param]
+        else:
+            num_filters = len(params[param].keys())
+            for index, filterField in enumerate(params[param]):
+                filter_values = " ".join(['"{}"'.format(x) for x in params[param][filterField]])
+                param_value += "{}:({})".format(filterField, filter_values)
+                if index < num_filters-1:
+                    param_value += ' AND '
+        url += "{}={}".format(param, param_value)
+        if idx < num_params-1:
+            url += "&"
+    return url
+
+
+for k in range(end_year - start_year):
+    # For each year, find articles for each quarter
+    quarter_start = ["0101", "0401", "0701", "1001"]
+    quarter_end = ["0331", "0630", "0930", "1231"]
+    for quarter in range(4):
+        # if (start_year+k >= 2007) and (start_year+k <= 2007):
+        #     if quarter < 3:
+        #         continue
+        begin_date = "{}{}".format(start_year+k, quarter_start[quarter])
+        end_date = "{}{}".format(start_year+k, quarter_end[quarter])
+
+        queryURL = generateURL(url,params)
+        queryURL += "&begin_date={}&end_date={}".format(begin_date, end_date)
+
+        # Loop through all pages (each page has data of 10 articles)
+        api_swapper = 1
+        current_api_key = api_keys[api_swapper]
+
+        # Fet the data first to get the total number of hits
+        print("{} - {}".format(start_year+k, quarter+1))
+        try:
+            response = (requests.get(queryURL)).json()
+            # If some issue with response, switch the API key
+            while True:
+                if 'response' not in response:
+                    api_swapper = (api_swapper + 1) % len(api_keys)
+                    queryURL = queryURL.replace(current_api_key, api_keys[api_swapper])
+                    current_api_key = api_keys[api_swapper]
+                    time.sleep(0.8)
+                    response = (requests.get(queryURL)).json()
+                else:
+                    break
+            pages = min(200, math.ceil(response['response']['meta']['hits']/10))
+            pbar = tqdm(total=pages) # Only to show the progress bar
+
+            articles_meta_data = []
+            file_count = 1
+            
+            # Keep track of duplicate articles
+            duplicate_articles = []
+            duplicate_articles_url = []
+            
+            with open('data/articles_metadata_{}_{}.csv'.format(start_year+k, quarter+1), 'w') as output_file:
+                for i in range(pages):
+                    current_url = queryURL + '&page={}'.format(i)
+                    response = (requests.get(current_url)).json()
+                    # If some issue with response, switch the API key
+                    while True:
+                        if 'response' not in response:
+                            api_swapper = (api_swapper + 1) % len(api_keys)
+                            queryURL = queryURL.replace(current_api_key, api_keys[api_swapper])
+                            current_api_key = api_keys[api_swapper]
+                            time.sleep(0.8)
+                            response = (requests.get(queryURL)).json()
+                        else:
+                            break
+                    articles = response['response']['docs']
+                    for article in articles:
+                        articles_meta_data.append({
+                            "_id": article["_id"],
+                            "url": article["web_url"], # URL of the article
+                            "word_count": article["word_count"], # Number of words in the article
+                            "section": article["section_name"], # News paper section under which the article is printed 
+                            "date": article["pub_date"], # Article published date
+                            "type": article["news_desk"], # Type of topic (business, sports, economic etc)
+                            "headline": article["headline"]["main"].replace('\n', ' '), # Main headline of the article
+                            "abstract": article["abstract"].replace('\n', ' ')
+                        })
+                    
+                    # Update the progress bar
+                    time.sleep(0.1)
+                    pbar.update(1)
+
+                    # Keep swapping API keys
+                    api_swapper = (api_swapper + 1) % len(api_keys)
+                    queryURL = queryURL.replace(current_api_key, api_keys[api_swapper])
+                    current_api_key = api_keys[api_swapper]
+                    time.sleep(0.8)
+                keys = articles_meta_data[0].keys()
+                writer = csv.DictWriter(output_file, keys)
+                writer.writeheader()
+                writer.writerows(articles_meta_data)
+                pbar.close()
+        except:
+            print("Something went wrong while fetching the data! \n")
+            print(sys.exc_info())
+            print(response)
+            raise
