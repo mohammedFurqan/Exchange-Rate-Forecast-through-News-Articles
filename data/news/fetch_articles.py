7e7733de156469acd28f847d0c047287247e29cc
Mohammed Furqan Rahamath M <mohammed@moonraft.com>
2020-03-24 22:23:17 -0400

    Feat: Re-written script to crawl articles with multiprocessing

--- data/news/fetch_articles.py
+++ data/news/fetch_articles.py
@@ -3,7 +3,7 @@
 # 
 # Purpose: Fetch the articles content for corresponding meta-data
 
-
+import multiprocessing as mp
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
@@ -18,10 +18,35 @@ start_year = 1981
 end_year = 2019
 
 # Customise which year, quarter and row to start from and which year to end at
-start_from_year = 2011
+start_from_year = 2014
 start_from_quarter = 1
-start_from_row = 298
+start_from_row = 1
 stop_at_year = 2020
+
+
+def worker(row, q):
+    row_text = []
+    page = requests.get(row["url"]).text
+    soup = BeautifulSoup(page, 'html.parser')
+    article_body = soup.select('section[name=articleBody] p')
+    if len(article_body) == 0:
+        article_body = soup.select('p.story-body-text')
+    content = ""
+    for paragraph in article_body:
+        text_content = (paragraph.get_text()).strip()
+        if not text_content:
+            continue
+        content += (' ' + text_content.replace('\n', ''))
+    row['article'] = content
+    row_text = [row['_id'],row['url'],row['word_count'],row['section'],row['date'],row['type'],row['headline'].replace('\n', ''),row['abstract'].replace('\n', ''),row['article'].replace('\n', '')]
+    q.put(row_text)
+    return row_text
+
+def listener(q):
+    while 1:
+        m = q.get()
+        if m == 'kill':
+            break
 
 for year in range(end_year - start_year):
     # Stop at the mentioned year
@@ -51,20 +76,27 @@ for year in range(end_year - start_year):
             with open('meta_data/articles_metadata_{}_{}.csv'.format(start_year+year, quarter+1)) as meta_file:
                 data = csv.DictReader(meta_file)
                 num_articles = 2000
-                print('{} - {}'.format(start_year+year, quarter+1))
-                pbar = tqdm(total=num_articles) # Only to show the progress bar
-                for index, row in enumerate(data):
-                    # Skip rows as per the customisations
-                    if (start_year+year == start_from_year) and (quarter == start_from_quarter-1) and (index < start_from_row):
-                        pbar.update(1)
+                manager = mp.Manager()
+                q = manager.Queue()    
+                pool = mp.Pool(10)
+                
+                #put listener to work first
+                watcher = pool.apply_async(listener, (q,))
+                
+                #fire off workers
+                jobs = []
+                for row in data:
+                    job = pool.apply_async(worker, (row, q))
+                    jobs.append(job)
+                
+                # collect results from the workers through the pool result queue
+                for job in jobs:
+                    row_val = job.get()
+                    if not row_val:
                         continue
-                    page = requests.get(row["url"]).text
-                    soup = BeautifulSoup(page, 'html.parser')
-                    article_body = soup.select('section[name=articleBody] p')
-                    content = ""
-                    for paragraph in article_body:
-                        content += (' ' + paragraph.get_text()).replace('\n', '')
-                    row['article'] = content
-                    pbar.update(1)
-                    writer.writerow([row['_id'],row['url'],row['word_count'],row['section'],row['date'],row['type'],row['headline'].replace('\n', ''),row['abstract'].replace('\n', ''),row['article']])
-                pbar.close()
+                    writer.writerow(row_val)
+                    
+                #now we are done, kill the listener
+                q.put('kill')
+                pool.close()
+                pool.join()
